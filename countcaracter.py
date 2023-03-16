6c2971e8ce7c91c0dc01b3c85d321d3c31751fae
EPAM\Veranika_Kachubeyeva <veranika_kachubeyeva@epam.com>
2023-03-16 17:53:23 +0300

    code cleanup

--- countcaracter.py
+++ countcaracter.py
@@ -1,22 +1,21 @@
 from collections import Counter
 import sys
-#import io
+
 
 filePath = sys.argv[1]
 
-print(filePath)
-print('cmd entry:', sys.argv)
+#print(filePath)
+#print('cmd entry:', sys.argv)
 f = open(filePath, "rb")
 #f = io.open(filePath, "r")  #, encoding='utf-8')
 
 d = f.read()
-print(d)
+#print(d)
 
-counter = dict(Counter((d)).most_common(10))
+counter = dict(Counter(d).most_common(10))
 print("COUNTER", counter)
 
 for key in counter:
-    #bkey = bytes(key)
     print(key, "=>", counter[key], "times")
 
 f.close()
