79ef5fc2090ece80616b585ef12b4dcebee7dae1
EPAM\Veranika_Kachubeyeva <veranika_kachubeyeva@epam.com>
2023-03-17 16:12:45 +0300

    added with, deleted try-except-finally

--- countcaracter.py
+++ countcaracter.py
@@ -3,21 +3,15 @@ import sys
 
 
 filePath = sys.argv[1]
-f = None
 
 
-try:
-    f = open(filePath, "rb")
+with open(filePath, "rb") as f:
     d = f.read()
     text = d.decode("utf8", errors="ignore")
     counter = Counter(text).most_common(10)
     print("COUNTER", counter)
     for key in counter:
         print(repr(key[0]), "=>", key[1], "times")   
-    f.close()
-except:
-    f.close()
-finally:
-    f.close()
+    
     
     
