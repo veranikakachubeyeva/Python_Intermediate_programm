110731eec9cf2d7eedf824f391f5bd1728fb67e6
EPAM\Veranika_Kachubeyeva <veranika_kachubeyeva@epam.com>
2023-03-17 15:13:23 +0300

    added try-except-finally, deleted dict, added repe(), deleted codecs

--- countcaracter.py
+++ countcaracter.py
@@ -1,20 +1,23 @@
 from collections import Counter
 import sys
-import codecs
 
 
 filePath = sys.argv[1]
+f = None
 
 
-f = open(filePath, "rb")
-d = f.read()
-text = codecs.decode(d, "utf8", errors="ignore")
-counter = dict(Counter(text).most_common(10))
-print("COUNTER", counter)
-
-for key in counter:
-    print(str(key.encode("utf8"))[1:], "=>", counter[key], "times")
-    #print(len(str(key.encode("utf8"))[1:]))
-
-
-f.close()
+try:
+    f = open(filePath, "rb")
+    d = f.read()
+    text = d.decode("utf8", errors="ignore")
+    counter = Counter(text).most_common(10)
+    print("COUNTER", counter)
+    for key in counter:
+        print(repr(key[0]), "=>", key[1], "times")   
+    f.close()
+except:
+    f.close()
+finally:
+    f.close()
+    
+    
