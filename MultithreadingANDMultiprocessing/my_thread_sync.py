f3e1ec3e6cce642a211430624cf44156755cc29b
EPAM\Veranika_Kachubeyeva <veranika_kachubeyeva@epam.com>
2023-07-17 16:04:43 +0300

    task 1 Multithreading topic

--- MultithreadingANDMultiprocessing/my_thread_sync.py
+++ MultithreadingANDMultiprocessing/my_thread_sync.py
@@ -1,37 +1,38 @@
-#Multithreading/processing: 
-#Write a Python script my_thread_sync.py which will have two threads 
-# (apart from main thread) printing numbers from 1 to 100 (inclusively). 
-#  The first thread must print only even numbers, the second thread must print only odd numbers. 
-# They must print numbers one after another so you will get correct sequence: 
-
-
 import threading
 
 
-even_semaphore = threading.Semaphore(1)
-odd_semaphore = threading.Semaphore(0)
+lock = threading.Lock()
 
+flag = 1
 
-def func_print_even_numbers():           
-    for num in range(2, 101, 2): 
-        odd_semaphore.acquire()       
-        print(num)        
-        even_semaphore.release()
+def func_print_even_numbers():
+    global flag
+    for num in range(2, 101, 2):        
+        while not flag == 0:            
+            pass
+        lock.acquire()          
+        print(num)
+        flag = 1
+        lock.release()
+        
+def func_print_odd_numbers():
+    global flag
+    for num in range(1, 100, 2):
+        while not flag == 1:           
+            pass
+        lock.acquire()          
+        print(num)
+        flag = 0
+        lock.release()
 
-
-def func_print_odd_numbers():            
-    for num in range(1, 100, 2): 
-        even_semaphore.acquire()       
-        print(num)        
-        odd_semaphore.release()
-
-if __name__ == "__main__":    
-          
+if __name__ == "__main__":
+    
     t1 = threading.Thread(target=func_print_even_numbers)
-    t2 = threading.Thread(target=func_print_odd_numbers) 
- 
+    t2 = threading.Thread(target=func_print_odd_numbers)      
+    
     t1.start()
-    t2.start()
- 
-    t1.join()
-    t2.join()
+    t2.start() 
+    
+    t1.join()  
+    t2.join()     
+    
