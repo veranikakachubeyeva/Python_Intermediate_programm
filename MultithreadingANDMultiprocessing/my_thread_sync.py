1d6497c02e13847ba300890bdace8e73e66defa4
EPAM\Veranika_Kachubeyeva <veranika_kachubeyeva@epam.com>
2023-07-19 16:19:19 +0300

    task 1 Multithreading with Barrier

--- MultithreadingANDMultiprocessing/my_thread_sync.py
+++ MultithreadingANDMultiprocessing/my_thread_sync.py
@@ -1,36 +1,38 @@
+#Multithreading/processing: 
+#Write a Python script my_thread_sync.py which will have two threads 
+# (apart from main thread) printing numbers from 1 to 100 (inclusively). 
+#  The first thread must print only even numbers, the second thread must print only odd numbers. 
+# They must print numbers one after another so you will get correct sequence: 
+
 import threading
+import time
 
-lock = threading.Lock()
+max_number = 100
 
-current_number = 1
+barrier = threading.Barrier(2)
 
-def func_print_even_numbers():
-    global current_number
-    while current_number <= 99:
-        with lock:
-            if current_number % 2 == 0:
-                print(current_number)
-                current_number += 1
-        
-def func_print_odd_numbers():
-    global current_number
-    while current_number <= 99:
-        lock.acquire()
-        try:
-            if current_number % 2 == 1:
-                print(current_number)
-                current_number += 1
-        finally:
-            lock.release()
+def print_even_numbers():
+    for i in range(2, max_number + 1, 2):               
+        time.sleep(0.1)
+        barrier.wait()
+        print(i)
+        print("//////////")
+
+def print_odd_numbers():
+    for i in range(1, max_number + 1, 2):            
+        time.sleep(0.2)
+        print(i)        
+        barrier.wait()
+        print("*************") 
+
 
 if __name__ == "__main__":
     
-    t1 = threading.Thread(target=func_print_even_numbers)
-    t2 = threading.Thread(target=func_print_odd_numbers)      
+    t1 = threading.Thread(target=print_odd_numbers)
+    t2 = threading.Thread(target=print_even_numbers)
     
     t1.start()
-    t2.start() 
-    
-    t1.join()  
-    t2.join()     
-    
+    t2.start()             
+        
+    t1.join()
+    t2.join()
